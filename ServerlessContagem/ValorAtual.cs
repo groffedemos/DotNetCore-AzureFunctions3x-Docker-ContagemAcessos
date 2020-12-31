a1b732c8a1d74a846fed871d2103dd1379942bd6
Renato Groffe <renato.groffe@yahoo.com.br>
2020-12-31 17:41:11 -0300

    Update ValorAtual.cs

--- ServerlessContagem/ValorAtual.cs
+++ ServerlessContagem/ValorAtual.cs
@@ -30,7 +30,7 @@ namespace ServerlessContagem
                     _CONTADOR.Local,
                     _CONTADOR.Kernel,
                     _CONTADOR.TargetFramework,
-                    MensagemFixa = "Teste no Azure... 1 2 3 4... Nono teste",
+                    MensagemFixa = "Teste no Azure... 1 2 3 4... Decimo teste",
                     MensagemVariavel = Environment.GetEnvironmentVariable("MensagemVariavel")
                 });
             }
